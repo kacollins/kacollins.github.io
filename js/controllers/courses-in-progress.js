da1c4f09649bdaba33ff813e9b1259d83258dded
kacollins <kimberlyacollins@gmail.com>
2017-03-11 10:40:23 -0600

    Refactored code for courses in progress.

--- js/controllers/courses-in-progress.js
+++ js/controllers/courses-in-progress.js
@@ -8,32 +8,10 @@ app.controller("coursesInProgressCtrl", function ($scope, $resource)
 
     CodeSchoolAPI.get({}, function (data)
     {
-        var coursesInProgressWithLevelsCompleted = data.courses.in_progress;
+        var coursesInProgress = getCoursesInProgress(data);
 
-        var regex = /large_badge\/(\d+)\//;
-
-        coursesInProgressWithLevelsCompleted.forEach(function (course)
-        {
-            course.levelsCompleted = data.badges
-                .filter(function (badge)
-                {
-                    return badge.name.indexOf("Level") >= 0
-                        && badge.course_url == course.url;
-                });
-
-            course.levelsCompleted.forEach(function(badge){
-                badge.level = badge.name.replace(" on " + course.title, "");
-
-                var match = regex.exec(badge.badge);
-
-                if (match)
-                {
-                    badge.badge_url = 'https://www.codeschool.com/users/kacollins/badges/' + match[1];
-                }
-            });
-        });
-
-        $scope.coursesInProgressWithLevelsCompleted = coursesInProgressWithLevelsCompleted.filter(function (course)
+        //Don't show courses that I just barely started and then decided not to do
+        $scope.coursesInProgressWithLevelsCompleted = coursesInProgress.filter(function (course)
         {
             return course.levelsCompleted.length > 0;
         });
@@ -41,3 +19,63 @@ app.controller("coursesInProgressCtrl", function ($scope, $resource)
         $scope.coursesInProgressCount = $scope.coursesInProgressWithLevelsCompleted.length;
     });
 });
+
+var getBadgeLevel = function (badge, course)
+{
+    var regex = /Level \d+/;
+    var level = "";
+
+    var match = regex.exec(badge.name);
+
+    if (match)
+    {
+        level = match[0];
+    }
+    else
+    {
+        level = badge.name.replace(" on " + course.title, "");
+    }
+
+    return level;
+};
+
+var getBadgeURL = function (badgeImageURL)
+{
+    var regex = /large_badge\/(\d+)\//;
+    var badgeURL = "";
+
+    var match = regex.exec(badgeImageURL);
+
+    if (match)
+    {
+        //match[0] is the entire match. match[1] is the capturing group (the badge ID)
+        badgeURL = 'https://www.codeschool.com/users/kacollins/badges/' + match[1];
+    }
+
+    return badgeURL;
+};
+
+var getCoursesInProgress = function (data)
+{
+    var coursesInProgress = data.courses.in_progress;
+
+    coursesInProgress.forEach(function (course)
+    {
+        course.levelsCompleted = data.badges
+            .filter(function (badge)
+            {
+                return badge.name.indexOf("Level") >= 0
+                    && badge.course_url == course.url;
+            });
+
+        course.levelsCompleted.map(function (badgeForCompletedLevel)
+        {
+            badgeForCompletedLevel.level = getBadgeLevel(badgeForCompletedLevel, course);
+            badgeForCompletedLevel.badge_url = getBadgeURL(badgeForCompletedLevel.badge);
+
+            return badgeForCompletedLevel;
+        });
+    });
+
+    return coursesInProgress;
+};
